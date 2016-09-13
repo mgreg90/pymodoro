5de18f70b5368ed95486e8ea83fee4b2fef5eed4
Michael Gregory <mgregory8219@gmail.com>
2016-09-13 00:06:37 -0400

    basic timer works. This is a fun little practice app.

--- pymodoro.py
+++ pymodoro.py
@@ -1,16 +1,60 @@
 import time, os, shutil
 
-timerLength = 25 * 60
-endTime = time.time() + timerLength
-(termWidth, termLength) = shutil.get_terminal_size()
-while time.time() < endTime:
-    now = time.time()
-    timer = round(endTime - now)
-    if(timer % 60 == 0):
-        clock = str(int(timer / 60)) + ":00"
-    else:
-        clock = str(int(timer / 60)) + ":" + str(timer % 60)
+def mainMenu():
     os.system("clear")
-    print("\n" * int(termLength / 4))
-    print(clock.center(termWidth))
-    time.sleep(1)
+    termSize = {}
+    [termSize['width'], termSize['length']] = shutil.get_terminal_size()
+    printTopQuarter("Welcome to Pymodoro!", termSize)
+    return termSize
+
+def options(termSize):
+    workTimer = printCenteredWithInput("How long should work periods be?", termSize)
+    breakTimer = printCenteredWithInput("How long should break periods be?", termSize)
+    timers = [workTimer, breakTimer]
+    return timers
+
+def printTopQuarter(text, size):
+    print("\n" * int(size['length'] / 4))
+    print(text.center(size['width']))
+
+def printCentered(text, size):
+    print(text.center(size['width']))
+
+def printCenteredWithInput(text, size):
+    return input(text.center(size['width']))
+
+def runClock(length, nextClockType):
+    timerLength = length
+    endTime = time.time() + timerLength
+    while time.time() < endTime:
+        now = time.time()
+        timer = round(endTime - now)
+        if(timer % 60 == 0):
+            clock = str(int(timer / 60)) + ":00"
+        elif(timer % 60 < 10):
+            clock = str(int(timer / 60)) + ":0" + str(timer % 60)
+        else:
+            clock = str(int(timer / 60)) + ":" + str(timer % 60)
+        os.system("clear")
+        printTopQuarter(clock, termSize)
+        time.sleep(1)
+    for x in range(0, 5):
+        print("\a")
+        time.sleep(0.15)
+    os.system("clear")
+
+    timerUpMsg1 = "Time is up!"
+    timerUpMsg2 = "Press Enter to begin " + nextClockType + " clock."
+    printTopQuarter(timerUpMsg1, termSize)
+    printCentered(timerUpMsg2, termSize)
+    printCenteredWithInput("Press ENTER", termSize)
+
+while True:
+    termSize = mainMenu()
+    timers = options(termSize)
+    print(timers)
+    input()
+    workTimer = timers[0]
+    breakTimer = timers[1]
+    runClock(int(float(workTimer) * 60), "break")
+    runClock(int(float(breakTimer) * 60), "pomodoro")
