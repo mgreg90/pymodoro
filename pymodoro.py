47d46c89963175d13d4a0c5c90b0a9ac6c463a02
Michael Gregory <mgregory8219@gmail.com>
2016-09-13 01:13:01 -0400

    pomodoro clock working nicely with options and quickpick

--- pymodoro.py
+++ pymodoro.py
@@ -11,6 +11,12 @@ def options(termSize):
     workTimer = printCenteredWithInput("How long should work periods be?", termSize)
     breakTimer = printCenteredWithInput("How long should break periods be?", termSize)
     timers = [workTimer, breakTimer]
+    timers = optionsQuickPick(timers)
+    return timers
+
+def optionsQuickPick(timers):
+    timers[0] = 25 if timers[0] == '' else timers[0]
+    timers[1] = 5 if timers[1] == '' else timers[1]
     return timers
 
 def printTopQuarter(text, size):
@@ -21,40 +27,68 @@ def printCentered(text, size):
     print(text.center(size['width']))
 
 def printCenteredWithInput(text, size):
-    return input(text.center(size['width']))
+    return input(text.center(size['width'])).center(size['width'])
 
-def runClock(length, nextClockType):
-    timerLength = length
+def runClock(timerLength, termSize, nextClockType):
     endTime = time.time() + timerLength
     while time.time() < endTime:
-        now = time.time()
-        timer = round(endTime - now)
-        if(timer % 60 == 0):
-            clock = str(int(timer / 60)) + ":00"
-        elif(timer % 60 < 10):
-            clock = str(int(timer / 60)) + ":0" + str(timer % 60)
-        else:
-            clock = str(int(timer / 60)) + ":" + str(timer % 60)
-        os.system("clear")
-        printTopQuarter(clock, termSize)
-        time.sleep(1)
+        runOneSecond(endTime, termSize)
     for x in range(0, 5):
-        print("\a")
-        time.sleep(0.15)
-    os.system("clear")
+        fiveBeeps()
+    clockExpired(nextClockType, termSize)
 
+def fiveBeeps():
+    print("\a")
+    time.sleep(0.15)
+
+def runOneSecond(endTime, termSize):
+    timer = setTimer(endTime)
+    clock = setClock(timer)
+    os.system("clear")
+    printTopQuarter(clock, termSize)
+    time.sleep(1)
+
+def setTimer(endTime):
+    now = time.time()
+    return round(endTime - now)
+
+def setClock(timer):
+    if(timer % 60 == 0):
+        clock = str(int(timer / 60)) + ":00"
+    elif(timer % 60 < 10):
+        clock = str(int(timer / 60)) + ":0" + str(timer % 60)
+    else:
+        clock = str(int(timer / 60)) + ":" + str(timer % 60)
+    return clock
+
+def clockExpired(nextClockType, termSize):
+    os.system("clear")
     timerUpMsg1 = "Time is up!"
-    timerUpMsg2 = "Press Enter to begin " + nextClockType + " clock."
+    if nextClockType == "break":
+        timerUpMsg2 = "Take a break!"
+    elif nextClockType == "pomodoro":
+        timerUpMsg2 = "Get to Work!"
+    timerUpMsg3 = "Press Enter to begin " + nextClockType + " clock."
     printTopQuarter(timerUpMsg1, termSize)
     printCentered(timerUpMsg2, termSize)
+    printCentered(timerUpMsg3, termSize)
+    print("\n")
     printCenteredWithInput("Press ENTER", termSize)
 
-while True:
+def startUp():
     termSize = mainMenu()
     timers = options(termSize)
-    print(timers)
-    input()
-    workTimer = timers[0]
-    breakTimer = timers[1]
-    runClock(int(float(workTimer) * 60), "break")
-    runClock(int(float(breakTimer) * 60), "pomodoro")
+    return [termSize, timers]
+
+def runTimers(timers, termSize):
+    workTimer = int(float(timers[0]) * 60)
+    breakTimer = int(float(timers[1]) * 60)
+    runClock(workTimer, termSize, "break")
+    runClock(breakTimer, termSize, "pomodoro")
+
+def runPymodoro():
+    [termSize, timers] = startUp()
+    while True:
+        runTimers(timers, termSize)
+
+runPymodoro()
